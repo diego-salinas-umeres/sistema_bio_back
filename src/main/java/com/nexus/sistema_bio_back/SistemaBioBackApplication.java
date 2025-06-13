c01bb11a1fa1d2738e39287ffa0cd2681c81c837
Diego S <salinas.umeres.da@gmail.com>
2025-06-13 00:55:42 -0500

    first commit

---
+++ src/main/java/com/nexus/sistema_bio_back/SistemaBioBackApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.sistema_bio_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBioBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus/sistema_bio_back/SistemaBioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.sistema_bio_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
