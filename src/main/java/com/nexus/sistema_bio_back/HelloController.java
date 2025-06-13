85d881bf08ab5e6a5670270ec14609b41f0d8896
Diego S <salinas.umeres.da@gmail.com>
2025-06-13 00:58:12 -0500

    prueba

---
+++ src/main/java/com/nexus/sistema_bio_back/HelloController.java
@@ -0,0 +1,16 @@
+package com.nexus.sistema_bio_back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hola, Spring Boot";
+    }
+}
